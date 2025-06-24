1e98f1c2dfeac0acb7d8428f7f629bc8b2513346
AdemArifoski <adem.arifoski8@gmail.com>
2025-06-24 22:05:00 +0200

    Add files via upload

--- memory-game/menu.js
+++ memory-game/menu.js
@@ -30,9 +30,6 @@ document.getElementById("hardButton").addEventListener("click", function() {
 
 
 
-
-
-
 // Show the settings panel when the settings icon is clicked
 const settingsIcon = document.querySelector(".settingsIcon");
 if (settingsIcon) {
@@ -47,6 +44,22 @@ document.querySelector(".closeButton").addEventListener("click", function () {
   
 });
 
+
+click = 0;
+// Increases height of settingspanel and displays credits when credits buttons is clicked
+document.getElementById("creditsButton").addEventListener("click", function(){
+  click++;
+  const settingsPanel = document.getElementById("settingsPanel");
+  const credits = document.getElementById("credits");
+  settingsPanel.style.height = "550px";
+  if(click % 2 !== 0) {
+    credits.style.display = "block";
+  } else {
+    credits.style.display = "none";
+    settingsPanel.style.height = "";
+  }
+  
+});
 
 
 const translations = {
@@ -64,7 +77,16 @@ const translations = {
     mediumDifficulty: "Medium",
     easyDifficulty: "Easy",
     selectTopic: "Select Topic",
-    selectNumber: "Numbers"
+    selectNumber: "Numbers",
+    credits1: "Naruto characters:",
+    credits2: "Used for a school project.",
+    credits3: "Found on Pinterest.",
+    credits4: "Found on Google Images.",
+    credits5: "Demon Slayer characters:",
+    credits6: "Number image:",
+    credits7: "Light and Dark Mode images",
+    credits8: "Credits",
+    credits9: "Image Credits"
     
   },
   de: {
@@ -81,7 +103,17 @@ const translations = {
     mediumDifficulty: "Mittel",
     easyDifficulty: "Einfach",
     selectTopic: "Thema auswählen",
-    selectNumber: "Zahlen"
+    selectNumber: "Zahlen",
+    credits1: "Naruto Charaktere:",
+    credits2: "Für ein Schulprojekt verwendet.",
+    credits3: "Auf Pinterest gefunden.",
+    credits4: "Auf Google Bilder gefunden.",
+    credits5: "Demon Slayer Charaktere:",
+    credits6: "Zahlenbild:",
+    credits7: "Bilder für Hell-/Dunkelmodus:",
+    credits8: "Credits",
+    credits9: "Bildnachweise"
+
   },
   fr: {
     preSettings: "Paramètres",
@@ -97,7 +129,17 @@ const translations = {
     mediumDifficulty: "Moyenne",
     easyDifficulty: "Facile",
     selectTopic: "Sélectionner un sujet",
-    selectNumber: "Nombres"
+    selectNumber: "Nombres",
+    credits1: "Personnages de Naruto:",
+    credits2: "Utilisé pour un projet scolaire.",
+    credits3: "Trouvé sur Pinterest.",
+    credits4: "Trouvé sur Google Images.",
+    credits5: "Personnages de Demon Slayer:",
+    credits6: "Image de chiffre:",
+    credits7: "Images du mode clair/sombre:",
+    credits8: "Crédits",
+    credits9: "Crédits d'image"
+
   }
 };
 
@@ -118,7 +160,20 @@ const selectTopics = [
   document.getElementById("selectTopic3")
 ];
 let numbers = document.getElementById("numbers");
-
+let creditsOnImages1 = document.getElementById("narutoCredits1");
+let creditsOnImages2 = document.getElementById("narutoCredits2");
+let creditsOnImages3 = document.getElementById("narutoCredits3");
+let creditsOnImages4 = document.getElementById("demonSlayerCredits1");
+let creditsOnImages5 = document.getElementById("demonSlayerCredits2");
+let creditsOnImages6 = document.getElementById("demonSlayerCredits3");
+let creditsOnImages7 = document.getElementById("numberCredits1");
+let creditsOnImages8 = document.getElementById("numberCredits2");
+let creditsOnImages9 = document.getElementById("numberCredits3");
+let creditsOnImages10 = document.getElementById("iightDarkCredits1");
+let creditsOnImages11 = document.getElementById("iightDarkCredits2");
+let creditsOnImages12 = document.getElementById("iightDarkCredits3");
+let imageCredits1 = document.getElementById("creditsButton");
+let imageCredits2 = document.getElementById("imageCredits");
 
 languageSelect.addEventListener("change", (e) => {
   setLanguage(e.target.value)
@@ -140,6 +195,20 @@ const setLanguage = (language) => {
       selectTopic.innerText = translations.en.selectTopic;
     });
     numbers.innerText = translations.en.selectNumber;
+    creditsOnImages1.innerText = translations.en.credits1;
+    creditsOnImages2.innerText = translations.en.credits2;
+    creditsOnImages3.innerText = translations.en.credits3;
+    creditsOnImages4.innerText = translations.en.credits5;
+    creditsOnImages5.innerText = translations.en.credits2;
+    creditsOnImages6.innerText = translations.en.credits3;
+    creditsOnImages7.innerText = translations.en.credits6;
+    creditsOnImages8.innerText = translations.en.credits2;
+    creditsOnImages9.innerText = translations.en.credits3;
+    creditsOnImages10.innerText = translations.en.credits7;
+    creditsOnImages11.innerText = translations.en.credits2;
+    creditsOnImages12.innerText = translations.en.credits4;
+    imageCredits1.innerText =  translations.en.credits8;
+    imageCredits2.innerText =  translations.en.credits9;
 
   } else if(language == "de"){
     preSeting.childNodes[0].nodeValue = translations.de.preSettings + " ";
@@ -156,6 +225,21 @@ const setLanguage = (language) => {
       selectTopic.innerText = translations.de.selectTopic;
     });
     numbers.innerText = translations.de.selectNumber;
+    creditsOnImages1.innerText = translations.de.credits1;
+    creditsOnImages2.innerText = translations.de.credits2;
+    creditsOnImages3.innerText = translations.de.credits3;
+    creditsOnImages4.innerText = translations.de.credits5;
+    creditsOnImages5.innerText = translations.de.credits2;
+    creditsOnImages6.innerText = translations.de.credits3;
+    creditsOnImages7.innerText = translations.de.credits6;
+    creditsOnImages8.innerText = translations.de.credits2;
+    creditsOnImages9.innerText = translations.de.credits3;
+    creditsOnImages10.innerText = translations.de.credits7;
+    creditsOnImages11.innerText = translations.de.credits2;
+    creditsOnImages12.innerText = translations.de.credits4;
+    imageCredits1.innerText =  translations.de.credits8;
+    imageCredits2.innerText =  translations.de.credits9;
+
   } else if(language == "fr"){
     preSeting.childNodes[0].nodeValue = translations.fr.preSettings + " ";
     settings.innerText = translations.fr.settingsTitle;
@@ -171,6 +255,20 @@ const setLanguage = (language) => {
       selectTopic.innerText = translations.fr.selectTopic;
     });
     numbers.innerText = translations.fr.selectNumber;
+    creditsOnImages1.innerText = translations.fr.credits1;
+    creditsOnImages2.innerText = translations.fr.credits2;
+    creditsOnImages3.innerText = translations.fr.credits3;
+    creditsOnImages4.innerText = translations.fr.credits5;
+    creditsOnImages5.innerText = translations.fr.credits2;
+    creditsOnImages6.innerText = translations.fr.credits3;
+    creditsOnImages7.innerText = translations.fr.credits6;
+    creditsOnImages8.innerText = translations.fr.credits2;
+    creditsOnImages9.innerText = translations.fr.credits3;
+    creditsOnImages10.innerText = translations.fr.credits7;
+    creditsOnImages11.innerText = translations.fr.credits2;
+    creditsOnImages12.innerText = translations.fr.credits4;
+    imageCredits1.innerText =  translations.fr.credits8;
+    imageCredits2.innerText =  translations.fr.credits9;
   }
 
 };    
